e741629711fa6c4333fa76e052268c47cfd57f49
Tyler <tyler.mackay@barclays.com>
2023-02-20 15:36:25 +0000

    completed part one

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
-const {Musician} = require("./Musician")
-const {sequelize} = require("./db")
+const { Musician } = require("./Musician");
+const { sequelize } = require("./db");
 
 const port = 3000;
 
-//TODO
+app.get("/musicians", async (req, res) => {
+  const musicians = await Musician.findAll();
+  res.json(musicians);
+});
 
 app.listen(port, () => {
-    sequelize.sync();
-    console.log(`Listening on port ${port}`)
-})
+  sequelize.sync();
+  console.log(`Listening on port ${port}`)
+});
